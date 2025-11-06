86b1dcdd4d022f34798509c44f23ecc1179a6c61
DavidRendonDev <davidrex114@hotmail.com>
2025-11-06 08:46:09 -0500

    base de datos funcional,problema de versionamiento

---
+++ school.Domain/Entities/Student.cs
@@ -0,0 +1,8 @@
+namespace school.Domain.Entities;
+
+public class Student 
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Phone { get; set; }
+}
